c6a0340ae1e20a852a36986b72450c9257728db9
24flopp <84596062+24flopp@users.noreply.github.com>
2023-01-15 18:27:24 +0100

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const unzipper = require('unzipper')
+const axios = require('axios')
 const path = require('path')
+
 module.exports = {
   name: 'setup',
   usage: 'bot [path]',
@@ -23,8 +25,14 @@ module.exports = {
     }
 
     try {
-      fs.createReadStream('https://github.com/24flopp/floppacli/raw/main/bot.zip')
-        .pipe(unzipper.Extract({ path: botPath }))
+      const url = 'https://github.com/24flopp/floppacli/raw/main/bot.zip'
+      const response = await axios({
+        method: 'GET',
+        url: url,
+        responseType: 'stream'
+      })
+
+      response.data.pipe(unzipper.Extract({ path: botPath }))
         .on('close', () => {
           console.log("Bot set up successfully in the specified path");
           prompt();
