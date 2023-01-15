13eac02bdfa9f47738c2937270910f23183de121
24flopp <84596062+24flopp@users.noreply.github.com>
2023-01-15 16:36:35 +0100

    Update touch.js

--- touch.js
+++ touch.js
@@ -1,10 +1,9 @@
-const { prompt } = require('../index.js')
 const fs = require('fs')
 module.exports = {
   usage: '<name>',
   name: 'touch',
   description: 'Create file.',
-  async execute(command){
+  async execute(command, prompt){
     const fileName = command.split(' ')[1]
     fs.closeSync(fs.openSync(fileName, 'w'));
 console.log(`File ${fileName} created.`);
